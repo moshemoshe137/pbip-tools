33766ad1e89cc5b6f051c88b5e06848ee70294e1
Moshe Rubin <mosherubin137@gmail.com>
2024-09-16 14:47:04 -0500

    Add original `clean_JSON` script

---
+++ pbi_pbip_filters/clean/clean_JSON.py
@@ -0,0 +1,26 @@
+import json
+import sys
+
+
+def format_nested_json_strings(json_data):
+    if not isinstance(json_data, dict | list):
+        return json_data
+
+    index = range(len(json_data)) if isinstance(json_data, list) else json_data.keys()
+    for list_position_or_dict_key in index:
+        value = json_data[list_position_or_dict_key]
+        if isinstance(value, dict | list):
+            json_data[list_position_or_dict_key] = format_nested_json_strings(value)
+        elif isinstance(value, str):
+            try:
+                parsed_value = json.loads(value)
+                formatted_value = format_nested_json_strings(parsed_value)
+                json_data[list_position_or_dict_key] = formatted_value
+            except json.JSONDecodeError:
+                continue
+    return json_data
+
+
+json_data = json.loads(sys.stdin.read())
+formatted_data = format_nested_json_strings(json_data)
+sys.stdout.write(json.dumps(formatted_data, ensure_ascii=False, indent=4))
