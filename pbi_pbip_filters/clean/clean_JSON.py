32dd548c1a9ca70e72e7a7b76b9b51a668fc9b83
Moshe Rubin <mosherubin137@gmail.com>
2024-09-17 16:55:52 -0500

    Improve list conversion (without casting to dict)

--- pbi_pbip_filters/clean/clean_JSON.py
+++ pbi_pbip_filters/clean/clean_JSON.py
@@ -9,20 +9,16 @@ def format_nested_json_strings(json_data: JSONType) -> JSONType:
     if not isinstance(json_data, dict | list):
         return json_data
 
-    if isinstance(json_data, list):
-        # Turn it into a `dict`.
-        json_data = dict(enumerate(json_data))
-
     index = range(len(json_data)) if isinstance(json_data, list) else json_data.keys()
     for list_position_or_dict_key in index:
-        value = json_data[list_position_or_dict_key]
+        value = json_data[list_position_or_dict_key]  # type: ignore[index]
         if isinstance(value, dict | list):
-            json_data[list_position_or_dict_key] = format_nested_json_strings(value)
+            json_data[list_position_or_dict_key] = format_nested_json_strings(value)  # type:ignore[index]
         elif isinstance(value, str):
             try:
                 parsed_value = json.loads(value)
                 formatted_value = format_nested_json_strings(parsed_value)
-                json_data[list_position_or_dict_key] = formatted_value
+                json_data[list_position_or_dict_key] = formatted_value  # type:ignore[index]
             except json.JSONDecodeError:
                 continue
     return json_data
