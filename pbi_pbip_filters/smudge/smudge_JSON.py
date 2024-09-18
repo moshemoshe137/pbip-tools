b08258546a580698673e51d2ac4f36dcfebaae5c
Moshe Rubin <mosherubin137@gmail.com>
2024-09-18 17:00:16 -0500

    Move the regex substitution inside `smudge_JSON`

--- pbi_pbip_filters/smudge/smudge_JSON.py
+++ pbi_pbip_filters/smudge/smudge_JSON.py
@@ -7,31 +7,64 @@ from pathlib import Path
 from pbi_pbip_filters.type_aliases import JSONType, PathLike
 
 
-def smudge_json(data: JSONType) -> JSONType:
+def smudge_json(data: JSONType) -> str:
     """
     Converts certain keys' values back to JSON strings for use by Power BI.
     """
 
-    # Define the keys that need to be converted to JSON strings
-    conditional_keys = {"config", "filters", "value", "parameters"}
+    def recursively_smudge_json(json_subset: JSONType) -> JSONType:
+        """
+        Converts certain keys' values back to JSON strings for use by Power BI.
+        """
+        # Define the keys that need to be converted to JSON strings
+        conditional_keys = {"config", "filters", "value", "parameters"}
 
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if key in conditional_keys and isinstance(value, dict | list):
-                # Convert these keys back to JSON strings
-                data[key] = json.dumps(
-                    value,
-                    ensure_ascii=False,
-                    indent=0,
-                    separators=(",", ":"),
-                ).replace("\n", "")
-            else:
-                # Recursively apply the smudge operation
-                data[key] = smudge_json(value)
-    elif isinstance(data, list):
-        data = [smudge_json(item) for item in data]
+        if isinstance(json_subset, dict):
+            for key, value in json_subset.items():
+                if key in conditional_keys and isinstance(value, dict | list):
+                    # Convert these keys back to JSON strings
+                    json_subset[key] = json.dumps(
+                        value,
+                        ensure_ascii=False,
+                        indent=0,
+                        separators=(",", ":"),
+                    ).replace("\n", "")
+                else:
+                    # Recursively apply the smudge operation
+                    json_subset[key] = recursively_smudge_json(value)
+        elif isinstance(json_subset, list):
+            json_subset = [recursively_smudge_json(item) for item in json_subset]
 
-    return data
+        return json_subset
+
+    # Recursively smudge the data
+    data = recursively_smudge_json(data)
+
+    # Final post-processing
+    data_str = json.dumps(data, ensure_ascii=False, indent=2)
+    data_str = re.sub(pattern_decimal_with_tenths_place_only, replacement, data_str)
+    return data_str  # noqa: RET504: "Unnecessary assignment to `data_str` before `return` statement"
+
+
+pattern_decimal_with_tenths_place_only = r"""(?x)  # (Turns on comments for this regex.)
+        (           # !!!!!!!!! Start Capturing Part 1 (text before the value) !!!!!!!!!
+            \s*?:       # Zero or more spaces followed by a colon.
+            \s*?        # There *might* be space after the colon.
+        )           # !!!!!!!! Finish Capturing Part 1 (text before the value) !!!!!!!!!
+
+        (?P<value>  # $$$$$$$$$$$$$$$$$$$$ Start Capturing "value" $$$$$$$$$$$$$$$$$$$$$
+            \d+         # One or more digits...
+            \.          # followed by a literal dot...
+            \d          # followed by exactly one digit in the tenths place.
+        )           # $$$$$$$$$$$$$$$$$$$ Finish Capturing "value" $$$$$$$$$$$$$$$$$$$$$
+
+        (           # %%%%%%%%% Start Capturing Part 3 (text after the value) %%%%%%%%%%
+            \s*?        # There *might* be space after the digit.
+            []},]       # Usually ends with ","; might end with "}" or "]".
+        )           # %%%%%%%% Finish Capturing Part 13 (text after the value) %%%%%%%%%
+        """
+
+replacement = r"\1\g<value>0\3"  # Tack on a zero in the thousandths place.
 
 
 def _format_json_files(json_files: Iterable[PathLike]) -> int:
@@ -41,26 +74,6 @@ def _format_json_files(json_files: Iterable[PathLike]) -> int:
                 cleaned_original_json = json.loads(f.read())
 
             smudged_json = smudge_json(cleaned_original_json)
-            smudged_json = json.dumps(smudged_json, ensure_ascii=False, indent=2)
-
-            # Force all floats to two decimal places with this regex
-            pat = r"""(?x)  # (Turns on comments for this regex.)
-                (           # ##### Start Capturing Part 1 (text before the value) #####
-                    \s*?:       # Zero or more spaces followed by a colon.
-                    \s*?        # There *might* be space after the colon.
-                )           # #### Finish Capturing Part 1 (text before the value) #####
-                (?P<value>  # ################ Start Capturing "value" #################
-                    \d+         # One or more digits...
-                    \.          # followed by a literal dot...
-                    \d          # followed by exactly one digit in the 10ths place.
-                )           # ############### Finish Capturing "value" #################
-                (           # ##### Start Capturing Part 3 (text after the value) ######
-                    \s*?        # There *might* be space after the digit
-                    []},]       # Usually ends with ","; might end with "}" or "]
-                )           # #### Finish Capturing Part 13 (text after the value) #####
-                """
-            replacement = r"\1\g<value>0\3"  # Tack on a zero in the 100ths place.
-            smudged_json = re.sub(pat, replacement, smudged_json)
 
             with Path(file).open("w", encoding="UTF-8") as f:
                 f.write(smudged_json)
