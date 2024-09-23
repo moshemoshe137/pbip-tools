a5143fe70b927eadc084d4f2ef17c60faee7f69b
Moshe Rubin <mosherubin137@gmail.com>
2024-09-23 15:19:07 -0500

    Add docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+"""Shared `pytest` fixtures."""
+
 import shutil
 from collections.abc import Callable, Iterator
 from pathlib import Path
@@ -18,12 +20,28 @@ json_files_list = list(top_level_directory.glob(json_glob))
 
 @pytest.fixture(params=json_files_list, ids=str)
 def json_files() -> list[Path]:
-    """All JSON files from the sample Power BI report."""
+    """
+    All JSON files from the sample Power BI report.
+
+    These JSON files are globbed from the project directory and point to the files in
+    the current project.
+    """
     return json_files_list
 
 
 @pytest.fixture
 def temp_json_files(json_files: list[Path], tmp_path: Path) -> Iterator[Path]:
+    """
+    All JSON files from the sample Power BI report in a temporary directory.
+
+    The JSON files are globbed from the project directory then copied to a temporary
+    directory. The refer to a copy of the file and not the original project file.
+
+    Returns
+    -------
+    Iterator[Path]
+        A list or iterator over the temporary JSON filepaths.
+    """
     for file in json_files:
         shutil.copy2(file, tmp_path / file.name)
     return ((tmp_path / file.name).resolve() for file in json_files)
@@ -31,15 +49,41 @@ def temp_json_files(json_files: list[Path], tmp_path: Path) -> Iterator[Path]:
 
 @pytest.fixture(params=json_files_list, ids=str)
 def json_file(request: pytest.FixtureRequest) -> Path:
-    """A single JSON file from the sample Power BI report."""
+    """
+    Return a single JSON file from the sample Power BI report.
+
+    Returns
+    -------
+    Path
+        Path to the JSON file in the test.
+    """
     return request.param
 
 
 @pytest.fixture
 def json_from_file_str(json_file: Path) -> str:
+    """
+    Return the content of a test JSON file as a string.
+
+    Returns
+    -------
+    str
+        The contents of the test JSON file as a string.
+    """
     return Path(json_file).read_text(encoding="UTF-8")
 
 
 @pytest.fixture(params=[clean_json, smudge_json])
 def filter_function(request: pytest.FixtureRequest) -> Callable[[JSONType], str]:
+    """
+    Fixture that provides either the `clean_json` or `smudge_json` function.
+
+    This parameterized fixture will alternate between the two filter functions. This
+    allows tests to apply both filters without duplicating test code.
+
+    Returns
+    -------
+    Callable[[JSONType], str]
+        The `clean_json` or `smudge_json` function.
+    """
     return request.param
